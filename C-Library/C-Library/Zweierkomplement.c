51303ac2c1a1e394d7d01883f9afdf20c1308014
Dario Camenzind <dario.camenzind@gmail.com>
2020-05-12 08:54:40 +0200

    Added Zweierkomplement

---
+++ C-Library/C-Library/Zweierkomplement.c
@@ -0,0 +1,44 @@
+#include "Zweierkomplement.h"
+#define MOVE_NEXT position--;zahl>>=1;
+/*
+	Berechnet das Zweierkomplement und schreibt es in einen string.
+
+	returns:
+		0x00: OK
+		0x01: Buffer zu klein
+*/
+uint8_t GetZweierkomplement(int8_t zahl, char* result, uint8_t bufferLength) 
+{
+	if (bufferLength < 8) 
+	{
+		return 0x01;
+	}
+	else
+	{
+		for (uint8_t i = 0; i < 8; i++)
+		{
+			result[i] = '0';
+		}
+	}
+
+	uint8_t mask = 0x01;
+	uint8_t position = 7;
+
+	while (!(zahl & mask) && position >= 0)
+	{
+		MOVE_NEXT
+	}
+
+	if (zahl & mask)
+	{
+		result[position] = '1';
+		MOVE_NEXT
+	}
+
+	while (zahl && (position + 1) != 0)
+	{
+		result[position] = zahl & mask ? '0' : '1';
+		MOVE_NEXT
+	}
+
+}
